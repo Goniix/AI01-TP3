e1eccd7610210774792e7e89b497a96016b139e3
Goniix <dragonixgold@gmail.com>
2025-10-09 15:22:08 +0200

    pas mal avancé, il manque les commentaires

--- TP3.c
+++ TP3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <TP3.h>
+#include "TP3.h"
 
 // Il veut juste que le rapport soit clait, 2 pages, on calcule la complexité, pas besoin de justifier les choix de fonction tant que ça marche
 // faire le diagrame de gant si on a le temps, ça donne des points bonnus, sinon il veut juste la trace d'execution
-t_processus *creer_processus(int pid, int arrivee, int duree)
+t_processus *processus_init(int pid, int arrivee, int duree)
 {
     t_processus *res = malloc(sizeof(t_processus));
     res->pid = pid;
@@ -14,26 +14,108 @@ t_processus *creer_processus(int pid, int arrivee, int duree)
     return res;
 }
 
-void free_processus(t_processus *p)
+void processus_free(t_processus *p)
 {
     if (p != NULL)
     {
-        free_processus(p->suivant);
+        processus_free(p->suivant);
         free(p);
     }
 }
 
-t_processus *charger_processus(char *nom_fichier, int *nb_processus)
+t_processus *processus_load(char *nom_fichier, int nb_processus)
 {
     FILE *file = fopen(nom_fichier, "r");
     t_processus *res = NULL;
     t_processus *last = NULL;
-    for (int i = 0; i < *nb_processus; i++)
+    for (int i = 0; i < nb_processus; i++)
     {
         char *res_str;
-        int res_str_size;
+        long unsigned int res_str_size;
         getline(&res_str, &res_str_size, file); // lire avec getline
-        // formater avec sscanf
+        int pid, arrive, duree;
+        sscanf(res_str, "%d %d %d", &pid, &arrive, &duree);
+
+        t_processus *new = processus_init(pid, arrive, duree);
+        if (res == NULL)
+        {
+            res = new;
+        }
+        else
+        {
+            last->suivant = new;
+        }
+        last = new;
     }
     fclose(file);
+    return res;
+}
+
+void processus_print(t_processus *process)
+{
+    if (process != NULL)
+    {
+
+        printf(" %d | %d | %d\n", process->pid, process->arrivee, process->duree);
+        processus_print(process->suivant);
+    }
+}
+
+FIFO *fifo_init()
+{
+    FIFO *res = malloc(sizeof(FIFO));
+    res->first = NULL;
+    res->last = NULL;
+    res->size = 0;
+    return res;
+}
+
+FIFO *fifo_init_from_process(t_processus *p)
+{
+    FIFO *res = fifo_init();
+    t_processus *elem = p;
+    while (elem != NULL)
+    {
+        fifo_enfiler(res, elem);
+        elem = elem->suivant;
+    }
+    return res;
+}
+
+void fifo_free(FIFO *file)
+{
+    processus_free(file->first);
+    free(file);
+}
+
+int fifo_vide(FIFO *file)
+{
+    return file->size == 0 ? 1 : 0;
+}
+
+FIFO *fifo_enfiler(FIFO *file, t_processus *p)
+{
+    if (fifo_vide(file))
+    {
+        file->first = p;
+        file->last = p;
+    }
+    else
+    {
+        file->last->suivant = p;
+        file->last = p;
+    }
+    file->size++;
+}
+
+int main()
+{
+    t_processus *open = processus_load("file", 3);
+    FIFO *fif = fifo_init_from_process(open);
+    processus_print(fif->first);
+    printf("\n");
+    processus_print(fif->last);
+    printf("size: %d\n", fif->size);
+
+    return 0;
 }
--- TP3.h
+++ TP3.h
@@ -1,3 +1,6 @@
+#ifndef TP3_H
+#define TP3_H
+
 typedef struct s_processus
 {
     int pid;                     // identifiant du processus
@@ -6,6 +9,23 @@ typedef struct s_processus
     struct s_processus *suivant; // pointeur vers le processus suivant
 } t_processus;
 
-t_processus *creer_processus(int pid, int arrivee, int duree);
-void free_processus(t_processus *p);
-t_processus *charger_processus(char *nom_fichier, int *nb_processus);
+t_processus *processus_init(int pid, int arrivee, int duree);
+void processus_free(t_processus *p);
+t_processus *processus_load(char *nom_fichier, int nb_processus);
+void processus_print(t_processus *process);
+
+typedef struct s_FIFO
+{
+    t_processus *first; // élément en tête de la queue
+    t_processus *last;  // dernier élément de la queue
+    int size;           // taille de la queue
+} FIFO;
+
+FIFO *fifo_init();
+FIFO *fifo_init_from_process(t_processus *p);
+void fifo_free(FIFO *file);
+int fifo_vide(FIFO *file);
+FIFO *fifo_enfiler(FIFO *file, t_processus *p);
+t_processus *fifo_defiler(FIFO *file);
+
+#endif
