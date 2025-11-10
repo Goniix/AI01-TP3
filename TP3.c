f9a4d1ecddab29a526656ae2bd3c260a795ddec7
Goniix <dragonixgold@gmail.com>
2025-11-10 16:57:51 +0100

    created main file

--- TP3.c
+++ TP3.c
@@ -474,24 +474,3 @@ void simuler_sjf(FIFO *tab)
         t++;
     }
 }
-
-int main()
-{
-    t_processus *open = NULL;
-    FIFO *fif = NULL;
-
-    open = processus_load("file_simple", 3);
-    fif = fifo_init_sorted_from_process(open, ARRIVEE);
-    fifo_print(fif);
-    printf("%d\n", fifo_is_sorted(fif, ARRIVEE));
-    fifo_free(&fif);
-
-    open = processus_load("file_simple", 3);
-    fif = fifo_init_from_process(open);
-    fifo_print(fif);
-    simuler_fcfs(fif);
-
-    fifo_free(&fif);
-
-    return 0;
-}
---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TP3.c"
+
+int main()
+{
+    t_processus *open = NULL;
+    FIFO *fif = NULL;
+
+    open = processus_load("file_simple", 3);
+    fif = fifo_init_sorted_from_process(open, ARRIVEE);
+    fifo_print(fif);
+    printf("%d\n", fifo_is_sorted(fif, ARRIVEE));
+    fifo_free(&fif);
+
+    open = processus_load("file_simple", 3);
+    fif = fifo_init_from_process(open);
+    fifo_print(fif);
+    simuler_fcfs(fif);
+
+    fifo_free(&fif);
+
+    return 0;
+}
